b445f433b8f0b8bcc9b736277121b26e3e9c9fd7
Makoto <makoto@mak.ru>
2024-02-26 05:01:36 +0300

    Ready-made functionality

--- src/main.py
+++ src/main.py
@@ -1,11 +1,33 @@
 from hh_parsing import HeadHunterInfo
 from vacancies import Vacancies
+from saving_information import SavingVacancies
 
 if __name__ == "__main__":
 
-    headhunter = HeadHunterInfo()
-    vacancies = Vacancies(headhunter.get_info())
+    hh_ru = HeadHunterInfo()
+    save = SavingVacancies(hh_ru.splitting_vacancies())
+    save.add_vacancies()
 
-    # print(headhunter.get_info()['items'])
-    print(headhunter.get_info()['items'])
+    vacancies_list = []
 
+    def user_interaction():
+        """
+        Функция для взаимодействия с пользователем.
+        Выводит отфильтрованную информацию о вакансиях
+        """
+
+        name = input("Введите название вакансии: ")  # Например: "На удалёнке"
+        top = int(input("Введите количество вакансий для проверки: "))  # Например: 8
+        requirement = input("Введите ключевые слова для фильтрации вакансий по требованиям: ")  # Например: "Способен работать в команде"
+        salary = input("Введите диапазон зарплаты: ")  # Например: "30000-40000"
+
+        vacancies = save.getting_data(name, top, salary, requirement)
+        save.delete_info()
+
+        for vacancy in vacancies:
+            information = Vacancies(vacancy)
+            vacancies_list.append(information)
+
+            print(f'{information}\n')
+
+    user_interaction()
